b1cd594ace0a1e2c98f0fa0ad9663584c7e3b09a
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2022-01-11 19:18:27 -0600

    trying to get this deployed

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
 
     const { fullName, username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "https://sunny-chat.vercel.app/auth";
+    const URL = "https://sunny-chat.vercel.app";
 
     const {
       data: { token, userId, hashedPassword },
