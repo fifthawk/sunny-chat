897d1b0071d99879add149dd1ace43445a137bb3
fifthawk <jackaallen@protonmail.com>
2022-01-11 19:32:47 -0600

    test

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
 
     const { fullName, username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "https://sunnychat.vercel.app/";
+    const URL = "https://sunnychat.vercel.app";
 
     const {
       data: { token, userId, hashedPassword },
