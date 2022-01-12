464689de4b46d730d7ed1f323ec98af9beb4d986
fifthawk <jackaallen@protonmail.com>
2022-01-11 22:07:47 -0600

    trying netlify

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
 
     const { fullName, username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "https://sunnychat.vercel.app/auth";
+    const URL = "sunny-chat.netlify.app";
 
     const {
       data: { token, userId, hashedPassword },
