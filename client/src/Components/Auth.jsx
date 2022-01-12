2f078ac2f38453417c9c11ce316b79bfb285422f
fifthawk <jackaallen@protonmail.com>
2022-01-12 17:31:23 -0600

    almost done

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
 
     const { fullName, username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "https://sunny-chat.herokuapp.com/authauth";
+    const URL = "https://sunny-chat.herokuapp.com/auth";
 
     const {
       data: { token, userId, hashedPassword },
